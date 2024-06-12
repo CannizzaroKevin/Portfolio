9cf4f2de10dd61f72832aa46acec3566f983062a
CannizzaroKevin <skylar74@outlook.fr>
2024-06-13 00:31:49 +0200

    add menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log("salut")
+const openMenu =() => {
+    const menu = document.querySelector(".header-menu");
+    menu.classList.toggle("active");
+    if(menu.classList.contains("active")){
+        document.querySelector(".header .material-icon").innerHTML = "close"
+}
+else {
+    document.querySelector(".header .material-icon").innerHTML = "manu"
+}
+}
